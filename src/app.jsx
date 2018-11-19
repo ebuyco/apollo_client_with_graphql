cfa18fcc18acb2cbebfb187f1cd612c668be70eb
ErnieBuyco <ernie.buyco@fmclarity.com>
2018-11-20 00:46:00 +0800

    update_version_1

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import ApolloCLient, { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloCLient({
+    uri: 'https://api-apeast.graphcms.com/v1/cjoli4udf1o2r01g5j2wdam9s/master'
+    
+});
+
+
 
 class App extends Component {
+
     render(){
         return(
-            <div>Hello</div>
+            <ApolloProvider client={client}>
+            <div>
+                <div>
+                   
+                </div>
+            </div>
+            </ApolloProvider>
         )
     }
 }
+
+
 
 export default App;
